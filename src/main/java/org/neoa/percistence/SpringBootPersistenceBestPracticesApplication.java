d5614cc81f5cdcaf523236080c1806f0eebfe22e
Cristian Flores <acrixf@gmail.com>
2022-02-22 20:31:06 -0600

    Initial commit

---
+++ src/main/java/org/neoa/percistence/SpringBootPersistenceBestPracticesApplication.java
@@ -0,0 +1,13 @@
+package org.neoa.percistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPersistenceBestPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPersistenceBestPracticesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/neoa/percistence/SpringBootPersistenceBestPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.neoa.percistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPersistenceBestPracticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
